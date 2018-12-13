1527c2a443d1d83d8c8ea9fe638172b6a8b5c4ae
Xiao Ling <lingxiao1002@gmail.com>
2018-12-13 09:59:11 +0800

    Updated summary

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dynamsoft:dbr',
   version: '6.4.1',
-  summary: 'Dynamsoft Barcode Reader JavaScript edition enables you to embed barcode reading functionality in your web app',
+  summary: 'A JavaScript barcode library that enables you to build web barcode reader app',
   git: 'https://github.com/dynamsoft-dbr/barcode-js-package.git',
   documentation: 'README.md'
 });
